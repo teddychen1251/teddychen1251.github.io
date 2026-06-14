49c031c819e214adbc6ab158b1fa0f0232b33d52
Teddy Chen <teddychen1251@gmail.com>
2026-06-13 21:22:40 -0700

    astro deployment setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://teddychen1251.github.io',
     integrations: [mdx(), sitemap(), react()],
     fonts: [
         {
